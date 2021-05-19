370e0a88c87d360f2d40ad968292aa01f6b96e64
Brinda-T <brindathanikanti1998@gmail.com>
2021-05-19 15:46:14 +0530

    changes to make text more readable

--- timeduration.py
+++ timeduration.py
@@ -1,10 +1,10 @@
-def time_date(str_new):
-    new_date, new_time = str_new.split('T')
-    print("new date:", new_date)
-    print("new time:", new_time)
-    yy, mm, dd = new_date.split('-')
+def get_time_date(str):
+    date, time = str.split('T')
+    print("date:", date)
+    print("time:", time)
+    yy, mm, dd = date.split('-')
     print(f"yy: {yy}, mm: {mm}, dd: {dd}")
-    hh, min, sec = new_time.split(':')
+    hh, min, sec = time.split(':')
     print(f"hh: {hh}, min: {min}, sec: {sec}")
     sec, msec = sec.split('.')
     print(f"sec: {sec}, msec: {msec}")
@@ -15,25 +15,27 @@ def time_date(str_new):
     return output
 
 def Extract_time(str_created_time, str_current_time):
-    a1, b1, c1, d1, e1, f1, g1 = time_date(str_created_time)
+    y1, m1, d1, h1, mn1, s1, ms1 = get_time_date(str_created_time)
     print()
-    a2, b2, c2, d2, e2, f2, g2 = time_date(str_current_time)
+    y2, m2, d2, h2, mn2, s2, ms2 = get_time_date(str_current_time)
 
-    #z1 = int(c1) - int(c2)
-    #z2 = ((12 - int(b1))-(12 - int(b2))) * 30
-    #z3 = (int(a2) - int(a1)) * 365
-    #result = z1 + z2 + z3
-    #z4 = (24 - int(d1)) - (24 - int(d2))
-    #z5 = (60 - int(e1)) - (60 - int(e2))
-    #z6 = (60 - int(f1)) - (60 - int(f2))
-    #print(result, ":days", z4, ":hrs", z5, ":min", z6, ":sec")
-    count1 = (int(a1) * 365 + int(b1) * 30 + int(c1))
-    count2 = (int(a2) * 365 + int(b2) * 30 + int(c2))
-    count3 = ((24 - int(d1)) + int(d1))
-    count4 = ((60 - int(e1)) + int(e2))
-    count5 = ((60 - int(f1)) + int(f2))
-    result = (count2 - count1, "days")
-    print("hrs", count3, end=" : ")
-    print("min", count4, end=" : ")
-    print("sec", count5)
+    date1 = (int(y1) * 365 + int(m1) * 30 + int(d1))
+    date2 = (int(y2) * 365 + int(m2) * 30 + int(d2))
+    hour = ((24 - int(h1)) + int(h1))
+    min = ((60 - int(mn1)) + int(mn2))
+    sec = ((60 - int(s1)) + int(s2))
+    result = (date2 - date1, "days")
+    print("hrs", hour, end=" : ")
+    print("min", min, end=" : ")
+    print("sec", sec)
+    print(result)
     return result
+
+def main():
+    str_created_time = "2017-2-29T08:30:27.11904211Z"
+    str_current_time = "2019-5-19T04:40:47.11904211Z"
+
+    Extract_time(str_created_time, str_current_time)
+
+if (__name__ == "__main__"):
+    main()
