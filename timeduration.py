f81480d0d05a5c061127c074773044d82bdb8882
Brinda-T <brindathanikanti1998@gmail.com>
2021-07-11 21:21:04 +0530

    updated code by modifying functions

--- timeduration.py
+++ timeduration.py
@@ -19,14 +19,6 @@ def Extract_time(str_created_time, str_current_time):
     print()
     a2, b2, c2, d2, e2, f2, g2 = time_date(str_current_time)
 
-    #z1 = int(c1) - int(c2)
-    #z2 = ((12 - int(b1))-(12 - int(b2))) * 30
-    #z3 = (int(a2) - int(a1)) * 365
-    #result = z1 + z2 + z3
-    #z4 = (24 - int(d1)) - (24 - int(d2))
-    #z5 = (60 - int(e1)) - (60 - int(e2))
-    #z6 = (60 - int(f1)) - (60 - int(f2))
-    #print(result, ":days", z4, ":hrs", z5, ":min", z6, ":sec")
     count1 = (int(a1) * 365 + int(b1) * 30 + int(c1))
     count2 = (int(a2) * 365 + int(b2) * 30 + int(c2))
     count3 = ((24 - int(d1)) + int(d1))
@@ -37,3 +29,12 @@ def Extract_time(str_created_time, str_current_time):
     print("min", count4, end=" : ")
     print("sec", count5)
     return result
+
+def main():
+    str_created_time = "2018-05-10T10:30:07.119045211Z"
+    str_current_time = "2020-12-29T10:30:07.119045211Z"
+    
+    Extract_time(str_created_time, str_current_time)
+
+if(__name__ == "__main__"):
+    main()
